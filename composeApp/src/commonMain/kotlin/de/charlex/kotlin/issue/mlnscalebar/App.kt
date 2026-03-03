eee506b59547ce59ba1d76b602ea41c2a2475c45
Alexander Karkossa <karkosal@MacBook-Pro-von-Alexander.local>
2026-03-03 09:11:07 +0100

    first commit

---
+++ composeApp/src/commonMain/kotlin/de/charlex/kotlin/issue/mlnscalebar/App.kt
@@ -0,0 +1,11 @@
+package de.charlex.kotlin.issue.mlnscalebar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.maplibre.compose.map.MaplibreMap
+
+@Composable
+@Preview
+fun App() {
+    MaplibreMap()
+}
